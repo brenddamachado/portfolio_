c572dbbea6f14492b3dcb2f558791951bea97810
brenddamachado <brenda16machado.bm@gmail.com>
2024-02-09 16:08:32 -0300

    botao mobile

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-document.getElementById("meuFormulario").addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita o envio padrão do formulário
-  
-    // Adicione aqui sua lógica de validação, se necessário
-  
-    // Exemplo de validação simples (substitua por sua lógica de validação real):
-    var isValid = true; // Supondo que o formulário é válido
-  
-    if (isValid) {
-      // Exibir o modal de sucesso
-      document.getElementById("modal").style.display = "block";
-    }
-  });
-  
-  // Fechar o modal ao clicar no botão de fechar
-  document.querySelector(".fechar").addEventListener("click", function () {
-    document.getElementById("modal").style.display = "none";
-  });
-  
+let bntMenu = document.getElementById("menu-icon")
+let menu = document.getElementById('menu-desktop')
+bntMenu.addEventListener('click', ()=>{
+  menu.classList.add("abrir-menu")
+})
